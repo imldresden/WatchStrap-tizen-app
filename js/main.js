2fdf762e86e3e50a71c01dc505614b2410a098a4
Tom Horak <dev@tom-horak.de>
2019-07-30 10:32:15 +0200

    Extended watch app. All events are now send.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-const identifier = "WATCH";
+const idMain = "MAIN";
+const idWatch = "WATCH";
+const idStrap = "STRAP";
+const idUpperStrap = "UPPER_STRAP";
+const idLowerStrap = "LOWER_STRAP";
 
 window.onload = () => {
 	tizen.power.request("SCREEN", "SCREEN_NORMAL");
 	document.body.style.backgroundColor = "gray";
+	
+	const canvas = document.getElementById('canvas');
 	const socket = io('http://141.76.67.139:31415');
+	
 
 	socket.on('connect', () => {
-		socket.emit('handshake', identifier);
+		socket.emit('handshake', idWatch);
 	});
 
 	socket.on('handshake', () => { document.body.style.backgroundColor = "green"; });
@@ -18,14 +25,84 @@ window.onload = () => {
 	});
 
 	socket.on('msg', msg => {
-	    if (msg.type === "imageData") {
-	    	var canavs = document.getElementById('canvas');
-	    	var ctx = canvas.getContext('2d');
-	    	var img = new Image;
-	    	img.onload = () => {
-	    		ctx.drawImage(img, 0, 0);
-	    	}
-	    	img.src = msg.payload;
+		switch (msg.type) {
+			case "imageData":
+				var ctx = canvas.getContext('2d');
+				var img = new Image;
+				img.onload = () => {
+					ctx.drawImage(img, 0, 0);
+				}
+				img.src = msg.payload;
+				break;
+			case "exit":
+				try { tizen.application.getCurrentApplication().exit(); }
+				catch (ignore) { }
+				break;
 	    }
 	});
+	
+	function sendEvent(type, event) {
+		socket.emit('msg', {
+			target: idMain,
+			sender: idWatch,
+			type: type,
+			payload: event
+		});
+	}
+	
+	function formatTouchEvent(event) {
+		console.log(event.touches.length, event);
+		let pos = {};
+		if (event.type === "touchend") {
+			pos.x = event.changedTouches[0].clientX;
+			pos.y = event.changedTouches[0].clientY;
+		} else {
+			pos.x = event.touches[0].clientX;
+			pos.y = event.touches[0].clientY;
+		}
+		var e = {
+			pos: pos,
+			timeStamp: event.timeStamp,
+			type: event.type
+		};
+		sendEvent('touch', e);
+	}
+	
+	function formatBezelEvent(event) {
+		var e = {
+			touches: event.touches,
+			timeStamp: event.timeStamp,
+			direction: event.detail.direction,
+			type: "bezelrotate"
+		};
+		sendEvent('bezelrotate', e);
+	}
+	
+	// Listener for hardware buttons
+	document.addEventListener('tizenhwkey', (event) => {
+		if (event.keyName === "back") {
+			try {
+				// tizenhwkey back event can also be triggered by a swipe down.
+				var e = {
+					key: event.keyName,
+					type: 'hwkey'
+				}
+				sendEvent('hwkey', e);
+			} catch (ignore) { }
+		}
+		
+		if (e.keyName === "menu") {
+			try { tizen.application.getCurrentApplication().exit(); }
+			catch (ignore) { }
+		}
+	});
+	
+	// Listener for rotatable bezel
+	document.addEventListener('rotarydetent', (e) => { formatBezelEvent(e); });
+	
+	// Listener for touch events
+	canvas.addEventListener('touchstart', (e) => { formatTouchEvent(e); });
+	canvas.addEventListener('touchmove', (e) => { formatTouchEvent(e); });
+	canvas.addEventListener('touchend', (e) => { formatTouchEvent(e); });
+	
 }
